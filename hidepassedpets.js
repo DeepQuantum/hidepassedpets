78fcdcb0ed5c778192c9ed2f639ce42975270199
Damian <damixd02@gmail.com>
2023-06-29 18:20:37 +0200

    1.0

--- hidepassedpets.js
+++ hidepassedpets.js
@@ -1,11 +1,29 @@
 const entries = document.getElementsByClassName("thing");
 const keywords = ["passed", "died", "dead", "rip", "loss", "grief", "grieving", "rainbow bridge", "hit by a car", "goodbye", "mourning"]
 
-for (const entry of entries) {
-    for (const keyword of keywords) {
-        if (entry.getElementsByClassName("title")[0].innerText.toLowerCase().includes(keyword)) {
-            entry.style.display = "none";
-            console.log("Hid post with keyword: " + keyword);
+function removeEntries() {
+    if (!enabled) return;
+    for (const entry of entries) {
+        for (const keyword of keywords) {
+            if (entry.getElementsByClassName("title")[0].innerText.toLowerCase().match(new RegExp("\\s" + keyword + "\\s", "g"))) {
+                entry.style.display = "none";
+            }
         }
     }
 }
+
+
+const targetNode = document.getElementById("siteTable");
+const config = { childList: true };
+
+const callback = function (mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            removeEntries();
+        }
+    }
+};
+
+removeEntries();
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
