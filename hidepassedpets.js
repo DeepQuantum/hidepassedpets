a592696f45139c847c04812c6b8021462a6d05a8
Damian <damixd02@gmail.com>
2023-06-30 05:16:08 +0200

    main functionality working

--- hidepassedpets.js
+++ hidepassedpets.js
@@ -1,22 +1,50 @@
 const entries = document.getElementsByClassName("thing");
-const keywords = ["passed", "died", "dead", "rip", "loss", "grief", "grieving", "rainbow bridge", "hit by a car", "goodbye", "mourning"]
-
+const keywords = ["passed", "die", "died", "dead", "rip", "loss", "lost", "grief", "grieving", "rainbow bridge", "hit by a car", "goodbye", "mourning"]
+let blockedEntries = new Array();
+let enabled = true;
 function removeEntries() {
-    if (!enabled) return;
     for (const entry of entries) {
         for (const keyword of keywords) {
-            if (entry.getElementsByClassName("title")[0].innerText.toLowerCase().match(new RegExp("\\s" + keyword + "\\s", "g"))) {
+            let title = entry.getElementsByClassName("title")[0].innerText.toLowerCase();
+            if (title.match(new RegExp("\\b" + keyword + "\\b", "g")) && !blockedEntries.includes(title)) {
                 entry.style.display = "none";
+                if (localStorage.getItem("blocked-passed-pets-posts") == null) {
+                    localStorage.setItem("blocked-passed-pets-posts", 1);
+                }
+                else {
+                    localStorage.setItem("blocked-passed-pets-posts", parseInt(localStorage.getItem("blocked-passed-pets-posts")) + 1);
+                }
+                blockedEntries.push(title);
             }
         }
     }
 }
 
 
+browser.runtime.onMessage.addListener(async (request) => {
+    if (request.unhide) {
+        length = blockedEntries.length;
+        unhideEntries();
+        blockedEntries = new Array();
+    }
+    else {
+        removeEntries();
+        length = blockedEntries.length;
+    }
+    let response = Promise.resolve({ count: length });
+    return response;
+});
+
+function unhideEntries() {
+    for (const entry of entries) {
+        entry.style.display = "block";
+    }
+}
+
 const targetNode = document.getElementById("siteTable");
 const config = { childList: true };
 
-const callback = function (mutationsList) {
+const callback = function (mutationsList, observer) {
     for (const mutation of mutationsList) {
         if (mutation.type === 'childList') {
             removeEntries();
